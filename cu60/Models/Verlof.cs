465d3ac887dd3dd01caef1765cc28d317af47983
IrisReijnen <ir.g.reijnen@gmail.com>
2023-10-04 14:42:59 +0200

    models

---
+++ cu60/Models/Verlof.cs
@@ -0,0 +1,12 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Verlof
+    {
+        public int ID { get; set; }
+        public DateOnly Date { get; set; }
+        public Status status { get; set; }
+        public VerlofReden reden { get; set; }
+        public Gebruiker gebruiker { get; set; }
+        public Gebruiker medewerker { get; set; }
+    }
+}
