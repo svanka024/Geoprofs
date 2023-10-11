4f0cc5629b78595a0a46255000f2db6d6fb28aa7
DoniBoye <donfokker@gmail.com>
2023-10-11 14:26:55 +0200

    commit 1
    pagina toegevoegt

---
+++ GeoProfs/Pages/VerlofGoedkeuren/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using GeoProfs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeoProfs.Pages.VerlofGoedkeuren
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
