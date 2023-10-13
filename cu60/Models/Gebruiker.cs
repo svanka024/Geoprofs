c77c73a7a619b7e04fb92b991a7f33ad8cb5eccc
IrisReijnen <ir.g.reijnen@gmail.com>
2023-10-13 14:58:44 +0200

    end of day

--- cu60/Models/Gebruiker.cs
+++ cu60/Models/Gebruiker.cs
@@ -3,10 +3,12 @@
     public class Gebruiker
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public int verlofSaldi { get; set; }
-        public Rechten rechten { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string BSN { get; set; }
+        public int VakantieDagen { get; set; }
+        public Positie Positie { get; set; }
+        public string opmerkingen { get; set; }
+        public DateOnly datumInDienst { get; set; }
     }
 }
