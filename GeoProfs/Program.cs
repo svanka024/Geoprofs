3e626c00ddafd32a77dc66cff0fa17904f33449b
IrisReijnen <ir.g.reijnen@gmail.com>
2023-10-25 16:12:55 +0200

    +

--- GeoProfs/Program.cs
+++ GeoProfs/Program.cs
@@ -6,10 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<GeoProfsContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GeoProfsContext") ?? throw new InvalidOperationException("Connection string 'GeoProfsContext' not found.")));
-//builder.Services.AddDbContext<GeoProfsContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("GeoProfsContext") ?? throw new InvalidOperationException("Connection string 'GeoProfsContext' not found.")));
-
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
