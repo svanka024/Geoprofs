7de13fe4c4c8219dcca04db9224a8a4881da00a2
DoniBoye <donfokker@gmail.com>
2023-11-03 15:23:53 +0100

    update

--- GeoProfs/Pages/ApproveLeave/Index.cshtml.cs
+++ GeoProfs/Pages/ApproveLeave/Index.cshtml.cs
@@ -6,9 +6,21 @@ namespace GeoProfs.Pages.VerlofGoedkeuren
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        //private readonly GeoProfs.Data.DbInitializer _context;
 
-        }
+        //public CreateModel(GeoProfs.Data.DbInitializer context)
+        //{
+        //    _context = context;
+        //}
+
+        //public IActionResult OnGet()
+        //{
+        //    Student = new Student { EnrollmentDate = DateTime.Now, FirstMidName = "Joe", LastName = "Smith" };
+        //    return Page();
+        //}
+
+        //[BindProperty]
+        //public Student Student { get; set; }
+
     }
 }
