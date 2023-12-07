958ecd0a2ad1aaf2e43f7be56d772ba830f18b3d
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-12-07 16:27:04 +0100

    moeite met php variabele als parameter voor javascript functie passen

--- GeoProfsNew/wwwroot/js/site.js
+++ GeoProfsNew/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// tab selection function for inbox page
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+//expandable table items for inbox page
+function cellselector(cellid) {
+    console.log(jscellid);
+    document.getElementById(test).style.backgroundColor = "blue";
+}
