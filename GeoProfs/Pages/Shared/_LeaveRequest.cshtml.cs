1098fe582bf7e48bf6c5c1b2728ff7b106cd3c20
DoniBoye <donfokker@gmail.com>
2023-12-21 15:08:25 +0100

    update

--- GeoProfs/Pages/Shared/_LeaveRequest.cshtml.cs
+++ GeoProfs/Pages/Shared/_LeaveRequest.cshtml.cs
@@ -7,20 +7,20 @@ using Microsoft.EntityFrameworkCore;
 namespace GeoProfs.Pages.Shared
 {
     public class _LeaveRequestModel : PageModel {
-        //private readonly GeoProfsContext _context;
+        private readonly GeoProfsContext _context;
 
-        //public _LeaveRequestModel(GeoProfsContext context)
-        //{
-        //    _context = context;
-        //}
+        public _LeaveRequestModel(GeoProfsContext context)
+        {
+            _context = context;
+        }
 
-        //public IList<LeaveRequest> LeaveRequest { get; set; } = default!;
+        public IList<LeaveRequest> LeaveRequest { get; set; } = default!;
 
-        //public async Task OnGetAsync()
-        //{
-        //    if (_context.LeaveRequests != null) {
-        //        LeaveRequest = await _context.LeaveRequests.Include(lr => lr.Reason).Include(lr => lr.Status).ToListAsync();
-        //    }
-        //}
+        public async Task OnGetAsync()
+        {
+            if (_context.LeaveRequests != null) {
+                LeaveRequest = await _context.LeaveRequests.Include(lr => lr.Reason).Include(lr => lr.Status).ToListAsync();
+            }
+        }
     }
 }
---
+++ GeoProfs/Pages/Shared/_Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeoProfs.Pages.Shared
+{
+    public class _TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
