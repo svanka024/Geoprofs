a1cf53b4160fa7946204224957c02891cbcfdb1b
DoniBoye <donfokker@gmail.com>
2023-12-21 15:45:23 +0100

    update

--- GeoProfs/Pages/Index.cshtml.cs
+++ GeoProfs/Pages/Index.cshtml.cs
@@ -1,20 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeoProfs.Data;
+using GeoProfs.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeoProfs.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
+        private readonly GeoProfsContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public IndexModel(GeoProfsContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+        public IList<LeaveRequest> LeaveRequest { get; set; } = default!;
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            if (_context.LeaveRequests != null) {
+                LeaveRequest = await _context.LeaveRequests.Include(lr => lr.Reason).Include(lr => lr.Status).ToListAsync();
+            }
         }
     }
 }
